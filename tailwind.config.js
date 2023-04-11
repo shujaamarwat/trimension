7e1e2ffee1535a78d56c19a3cf633070eb7d861a
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-04-11 22:59:54 +0500

    Added tailwind dependencies * package.json: Added reacter-router-dom and tailwind packages * tailwind.config.js: Added configuration settings

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'midnight': '#121212',
+      }
+    },
+  },
+  plugins: [],
+}
